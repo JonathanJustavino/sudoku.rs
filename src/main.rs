184bc6217c3279f47269aa23f7277c84f8d907ed
Jonathan L <s3190208@mail.zih.tu-dresden.de>
2023-03-27 17:10:53 +0200

    Move testing code to test module

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,4 @@ use crate::game_grid::Grid;
 fn main() {
     let grid = Grid{matrix: [[0;9]; 9]};
     println!("{}", grid);
-    let is_valid = grid.check_input_validity(&5, &(7, 8));
-    // println!("{}", is_valid);
 }
