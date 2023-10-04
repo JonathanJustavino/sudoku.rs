1a1e8403dd8013d992f6343c70c028a2e69670a4
Jonathan L <s3190208@mail.zih.tu-dresden.de>
2023-10-04 18:54:35 +0200

    Add neighbourhood generation

--- src/annealing/annealing_tests.rs
+++ src/annealing/annealing_tests.rs
@@ -3,8 +3,7 @@ use std::hash::Hash;
 
 #[cfg(test)]
 mod tests {
-    use crate::annealing::{generate_solution, annealing_tests::has_unique_elements};
-
+    use crate::annealing::{generate_solution, annealing_tests::has_unique_elements, generate_neighbourhood, swap};
 
     #[test]
     fn it_works() {
@@ -17,6 +16,28 @@ mod tests {
         let solution = generate_solution();
         assert_eq!(solution.len(), 9);
         assert!(has_unique_elements(solution));
+    }
+
+    #[test]
+    fn test_swap() {
+        let mut solution = generate_solution();
+        let check = solution.to_vec();
+        let (first, second) = swap(&mut solution);
+
+        assert_eq!(solution[first], check[second]);
+        assert_eq!(solution[second], check[first]);
+    }
+
+    #[test]
+    fn test_generate_neighbourhood() {
+        let solution = generate_solution();
+        let amount = 9;
+        let neighbours = generate_neighbourhood(solution, amount);
+        assert_eq!(neighbours.len(), amount as usize);
+
+        println!("{:?}", neighbours);
+
+        assert!(true);
     }
 }
 
--- src/annealing/mod.rs
+++ src/annealing/mod.rs
@@ -1,12 +1,40 @@
-use rand::{distributions::Uniform, Rng};
+use std::vec;
+use rand::Rng;
 
 #[cfg(test)]
 mod annealing_tests;
 
-pub fn generate_solution() -> Vec<usize> {
+pub fn generate_solution() -> Vec<u8> {
     let mut rng = rand::thread_rng();
-    let mut results = rand::seq::index::sample(&mut rng, 9, 9).into_vec();
-    results.iter_mut().for_each(|x| *x += 1);
+    let mut sampled = rand::seq::index::sample(&mut rng, 9, 9).into_vec();
+    sampled.iter_mut().for_each(|x| *x += 1);
+    let result = sampled.iter().map(|&e| e as u8).collect();
 
-    return results;
+    result
+}
+
+pub fn swap(solution: &mut Vec<u8>) -> (usize, usize) {
+    let mut rng = rand::thread_rng();
+    let first = rng.gen_range(1..9);
+    let second = rng.gen_range(1..9);
+    let first_value = solution[first];
+
+    solution[first] = solution[second];
+    solution[second] = first_value;
+
+    (first, second)
+}
+
+pub fn generate_neighbourhood(solution: Vec<u8>, amount: u8) -> Vec<Vec<u8>> {
+    let mut neighbourhood: Vec<Vec<u8>> = vec![];
+
+    for _ in 0..amount {
+        let mut neighbour = solution.to_vec();
+        swap(&mut neighbour);
+        let mut x = vec![neighbour];
+
+        neighbourhood.append(&mut x);
+    }
+
+    neighbourhood
 }
