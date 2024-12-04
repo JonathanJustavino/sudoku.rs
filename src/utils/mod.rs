b1fcbc38aa18a08e39547039fd108871b1babe06
Jonathan <jonathan.luederitz@gmx.de>
2024-12-04 12:41:32 +0100

    Add reading file reading functionality
    Sudoku templates can now be read from txt files stored in /static/

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,5 +1,8 @@
-use std::collections::HashSet;
+use std::{fs, path::PathBuf};
+use std::path::Path;
 use std::hash::Hash;
+use std::collections::HashSet;
+use ndarray::{array, Array2};
 
 
 pub fn has_unique_elements<T>(iter: T) -> bool
@@ -31,6 +34,42 @@ pub fn compute_standard_deviation(values: &[f64]) -> Option<f64> {
 
    let variance = sum_of_squares / (values.len() as f64);
    Some(variance.sqrt())
+}
+
+pub fn read_from_file(path: &PathBuf) -> String {
+    let read_op = fs::read_to_string(path);
+    let content = match read_op {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {error:?}"),
+    };
+    return  content;
+}
+
+fn cast_to_array(from_file: &PathBuf) -> Array2<u8> {
+    let content = read_from_file(from_file).replace('\n', "");
+
+    let tmp: Vec<u8> = content
+        .chars()
+        .map(|char| {
+            if char.is_ascii_digit() {
+                return (char as u8) - b'0';
+            } else {
+                panic!("Character is not a valid digit");
+            }
+        })
+        .collect();
+
+    let mut arr = Array2::<u8>::default((9, 9));
+    let mut row: usize = 0;
+    for (index, value) in tmp.iter().enumerate() {
+        if index != 0 && index % 9 == 0 {
+            row += 1;
+        }
+
+        arr[[row, index % 9]] = *value;
+    }
+
+    return arr;
 }
 
 #[cfg(test)]
--- src/utils/utils_tests.rs
+++ src/utils/utils_tests.rs
@@ -1,10 +1,64 @@
 #[cfg(test)]
 mod tests {
+    use crate::utils::{self, cast_to_array};
+    use std::path::{Path, PathBuf};
+    use std::env::current_dir;
+    use ndarray::{array, Array2, Axis};
+
+    fn create_template() -> Array2<u8> {
+       let data = array![
+            [0, 8, 9, 5, 4, 1, 6, 2, 0],
+            [0, 4, 2, 6, 9, 3, 7, 8, 0],
+            [6, 5, 3, 2, 8, 7, 4, 1, 9],
+            [9, 6, 4, 1, 2, 5, 3, 7, 8],
+            [8, 2, 1, 7, 3, 9, 5, 4, 6],
+            [3, 7, 5, 8, 6, 4, 2, 9, 1],
+            [2, 1, 7, 9, 5, 6, 8, 3, 4],
+            [4, 9, 6, 3, 7, 8, 1, 5, 2],
+            [5, 3, 8, 4, 1, 2, 9, 6, 7],
+        ];
+
+        return data;
+    }
+
+    fn create_testing_path() -> PathBuf {
+        let path_str = Path::new("static/off_by_one.txt");
+        let cur_dir = current_dir();
+        let base_path = match cur_dir {
+            Ok(path_buf) => path_buf,
+            Err(error) => panic!("Error trying to read from file: {error:?}"),
+        };
+
+        let base_path = base_path.as_path();
+        let sudoku_template_path_buf = base_path.join(path_str);
+        return sudoku_template_path_buf;
+    }
 
     #[test]
-    fn it_works() {
-        let result = 1 + 1;
-        assert_eq!(result, 2);
+    fn test_file_reading() {
+        let sudoku_template_path_buf = create_testing_path();
+        let contents = utils::read_from_file(&sudoku_template_path_buf);
+
+        assert!(Path::is_file(sudoku_template_path_buf.as_path()));
+        println!("{:?}", contents);
+    }
+
+    #[test]
+    fn test_cast_to_array() {
+        let sudoku_template_path_buf = create_testing_path();
+        let data = create_template();
+        let arr = cast_to_array(&sudoku_template_path_buf);
+
+        assert!(data == arr);
+    }
+
+    #[test]
+    fn test_template_correctness() {
+        let sudoku_template_path_buf = create_testing_path();
+        let data = create_template();
+        let arr = cast_to_array(&sudoku_template_path_buf);
+
+        assert!(data == arr);
     }
 
 }
